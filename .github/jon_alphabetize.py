2a06011771d3c0cc485250afdf17fdec977dbe62
jonbos-palmer <jbos@thepalmergroup.com>
2020-05-28 12:45:53 -0500

    Create jon_alphabetize.py

---
+++ .github/jon_alphabetize.py
@@ -0,0 +1,18 @@
+@@ -1,17 +0,0 @@
+# -*- coding: utf-8 -*-
+# @Author: Jon Bos
+# @Date:   2020-05-26
+# @Last Modified by:   jonbos
+# @Last Modified time: 2020-05-26
+# 
+
+import re
+
+with open("../README.md") as fin:
+    content = fin.readlines()
+
+content.sort(key = lambda line : re.findall('\[(.*?)\]', line))
+
+with open("README.md", "w") as fout:
+	for line in content:
+		fout.write(line)
